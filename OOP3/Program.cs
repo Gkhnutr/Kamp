7f1fa63c7c38d351823f4ffa856a11afaa9dccab
Gökhan Utar <g.utar308@gmail.com>
2023-09-05 18:59:31 +0300

    .

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -5,13 +5,16 @@ IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
 IKrediManager tasitKrediManager = new TasitKrediManager();
 IKrediManager konutKrediManager = new KonutKrediManager();
 IKrediManager esnafKredisiManager = new EsnafKredisiManager();
+
 ILoggerService databaseLoggerService = new DatabaseLoggerService();
 ILoggerService fileLoggerService = new FileLoggerService();
 ILoggerService smsLoggerService = new SmsLoggerService();
-List<ILoggerService> loggerServices = new List<ILoggerService>() { databaseLoggerService, smsLoggerService };
+
+List<ILoggerService> loggers = new List<ILoggerService>() { databaseLoggerService, smsLoggerService };
 
 BasvuruManager basvuruManager = new BasvuruManager();
-basvuruManager.BasvuruYap(esnafKredisiManager,loggerServices);
+
+basvuruManager.BasvuruYap(esnafKredisiManager,loggers);
 
 List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
 //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
